c8574a736f51861cc0afe7d4ce12d9b1519502a4
ViniDevelop <74693710+GDeveloperBR@users.noreply.github.com>
2022-03-30 17:31:52 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args)
+    {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("What do you want to do?\nPress 1 to create an account\nPress 2 to create a community\nPress 3 to edit your profile ");
+        int option = input.nextInt();
+
+        IFace_create_account iface = new IFace_create_account();
+        if(option == 1){
+            iface.createAccount();
+        }
+        else if(option == 2){
+            iface.addCommunity();
+        }
+        else if(option == 3){
+            iface.editProfile();
+        }
+        input.close();
+    }
+}
